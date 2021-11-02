4d2d916a1b169d3d38379a0b9381e03165fca51e
Anian Sollinger <49494079+WingsOfCobra@users.noreply.github.com>
2021-11-02 15:08:42 +0100

    login.py Hochgeladen
    Das login Skript "login.py" wurde hochgeladen. Es hat keinen Sinn. Es ist nur zur Übung von Inputs, If-abfragen und Variablen da!

---
+++ login.py
@@ -0,0 +1,35 @@
+#Wir erstellen einen Kunden login und eine Kunden account registrierung
+
+print("Willkommen im Room 847")
+
+#Es wird nach einer antwort gefragt
+answer = input("Haben sie bereits ein Konto? (ja oder nein)\n")
+
+#Wenn die antwort "ja" ist dann geht es zum login
+if(answer == "ja"):
+
+    print("\nWillkommen Zurück!")
+    
+    username = input("\nDein Benutzername: ")
+    password = input("Geben sie ihr Passwort ein: ")
+
+    print("\n" * 3 + "Willkommen im Room 847 " + username + "!")
+
+
+    
+#Wenn die antwort nicht ja war in der ersten abfrage: Versuche es mit elif und wenn die antwort nein ist geht es weiter mit der registrierung
+elif(answer == "nein"): 
+    
+    
+    print("\nHerzlich Willkommen. Erstelle bitte hier ein Konto!")
+
+    email = input("\nGeben sie eine E-Mail adresse ein: ")
+    username = input("Geben sie einen Benutzernamen ein: ")
+    password = input("Geben sie ein Passwort ein: ")
+
+    print("\n" *5 +  "Willkommen " + username + "! Dein Konto wurde erfolgreich mit der E-Mail adresse: " + email + " erstellt!")
+
+#Wenn die antwort nicht ja und nicht nein ist: Komme hier her und gebe einen Syntax error aus!
+elif(answer != "nein", answer != "ja"): {
+    print("Syntax error: try yes or no")
+}
